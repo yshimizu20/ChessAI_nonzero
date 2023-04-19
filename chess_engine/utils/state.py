156ee6029465f8932891d3cac433a707a8e1180f
yshimizu20 <yukohshimizu@icloud.com>
2023-04-19 23:09:03 +0900

    trying out non-zero version (more descriptive state representation

--- chess_engine/utils/state.py
+++ chess_engine/utils/state.py
@@ -3,13 +3,14 @@ import chess.pgn
 import numpy as np
 import random
 import torch
+import itertools
 
 from chess_engine.utils.utils import uci_dict, uci_table, piece_values
 
 
 def createStateObj(board):
     # convert state into a 8x8x12 tensor
-    state = torch.zeros((18, 8, 8), dtype=torch.float32)
+    state = torch.zeros((35, 8, 8), dtype=torch.float32)
 
     piece_map = board.piece_map()
     piece_types = [
@@ -37,6 +38,134 @@ def createStateObj(board):
     state[15, :, :] = float(p2_can_castle_kingside)
     state[16, :, :] = float(turn)
     state[17, :, :] = float(net_piece_value)
+
+    # enpassant
+    if board.ep_square is not None:
+        state[18, board.ep_square // 8, board.ep_square % 8] = 1.0
+
+    # squares white pawns can move to
+    from_mask = np.zeros(64, dtype=np.bool)
+    from_mask[list(board.pieces(chess.PAWN, chess.WHITE))] = 1
+    from_mask = int.from_bytes(np.packbits(from_mask), byteorder="big")
+    to_squares = [
+        move.to_square for move in board.generate_legal_moves(from_mask=from_mask)
+    ]
+    state[19, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black pawns can move to
+    from_mask = np.zeros(64, dtype=np.bool)
+    from_mask[list(board.pieces(chess.PAWN, chess.BLACK))] = 1
+    from_mask = int.from_bytes(np.packbits(from_mask), byteorder="big")
+    to_squares = [
+        move.to_square for move in board.generate_legal_moves(from_mask=from_mask)
+    ]
+    state[20, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white pawns can attack
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.PAWN, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[21, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black pawns can attack
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.PAWN, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[22, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white knights can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.KNIGHT, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[23, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black knights can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.KNIGHT, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[24, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white bishops can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.BISHOP, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[25, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black bishops can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.BISHOP, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[26, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white rooks can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.ROOK, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[27, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black rooks can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.ROOK, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[28, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white queens can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.QUEEN, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[29, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black queens can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.QUEEN, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[30, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares white king can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.KING, chess.WHITE))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[31, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # squares black king can move to
+    lst = [
+        list(board.attacks(square))
+        for square in list(board.pieces(chess.KING, chess.BLACK))
+    ]
+    to_squares = list(itertools.chain(*lst))
+    state[32, [sq // 8 for sq in to_squares], [sq % 8 for sq in to_squares]] += 1.0
+
+    # number of white attackers on each square
+    state[33, :, :] = torch.tensor(
+        [float(len(list(board.attackers(chess.WHITE, square)))) for square in range(64)]
+    ).reshape(8, 8)
+
+    # number of black attackers on each square
+    state[34, :, :] = torch.tensor(
+        [float(len(list(board.attackers(chess.BLACK, square)))) for square in range(64)]
+    ).reshape(8, 8)
 
     return state
 
